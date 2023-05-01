05683120460a2c1d5515f30725b037145184d4d5
Thanh Doan <doan.teddy@gmail.com>
2023-04-30 21:16:42 -0400

    added shapes and tests

--- test/shapes.test.js
+++ test/shapes.test.js
@@ -3,14 +3,14 @@ const { Square, Triangle, Circle } = require("./shapes");
 describe("Circle", () => {
   it("should render svg for a green circle element", () => {
     const expectedSvg = '<circle cx="150" cy="100" r="80" fill="green" />';
+    const newCircle = new Circle('green');
     // SOME VALUE - IS EXPECTED TO EQUAL - THE EXPECT VALUE
-
-
+    expect(expectedSvg.toEqual(newCircle.render()));
   });
   it("should accept a fillColor param", () => {
     const expectedSvg = '<circle cx="150" cy="100" r="80" fill="blue" />';
-
-
+    const newCircle = new Circle('blue');
+    expect(expectedSvg.toEqual(newCircle.render()));
   });
 });
 
@@ -18,14 +18,14 @@ describe("Triangle", () => {
   it("should render svg for a green polygon element", () => {
     const expectedSvg =
       '<polygon points="150, 18 244, 182 56, 182" fill="bisque" />';
-
-
+    const newTriangle = new Triangle('bisque');
+    expect(expectedSvg.toEqual(newTriangle.render()));
   });
   it("should accept a fillColor param", () => {
     const expectedSvg =
       '<polygon points="150, 18 244, 182 56, 182" fill="purple" />';
-
-
+    const newTriangle = new Triangle('purple');
+    expect(expectedSvg.toEqual(newTriangle.render()));
   });
 });
 
@@ -33,11 +33,13 @@ describe("Square", () => {
   it("should render svg for a green polygon element", () => {
     const expectedSvg =
       '<rect x="90" y="40" width="120" height="120" fill="dodgerblue" />';
-
+    const newSquare = new Square('dodgerblue');
+    expect(expectedSvg.toEqual(newSquare.render()));
   });
   it("should accept a fillColor param", () => {
     const expectedSvg =
       '<rect x="90" y="40" width="120" height="120" fill="red" />';
-
+    const newSquare = new Square('dodgerblue');
+    expect(expectedSvg.toEqual(newSquare.render()));
   });
 });
